76c219ebc0ad75be51a39ff1d2969be827ea462e
jonschlinkert <jon.schlinkert@sellside.com>
2017-04-23 01:38:39 -0400

    run update

--- index.js
+++ index.js
@@ -1,32 +1,23 @@
 /*!
  * deromanize <https://github.com/jonschlinkert/deromanize>
- * Based on http://bit.ly/1weeOfY
+ * Inspired by http://bit.ly/1weeOfY
  *
- * Copyright (c) 2014 Jon Schlinkert, contributors.
- * Licensed under the MIT License
+ * Copyright (c) 2014, 2017, Jon Schlinkert.
+ * Released under the MIT License.
  */
 
 'use strict';
+
+var numerals = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
 
 module.exports = function(roman) {
   roman = roman.toUpperCase().split('');
   var num = 0;
   var val = 0;
 
-  var lookup = {
-    I: 1,
-    V: 5,
-    X: 10,
-    L: 50,
-    C: 100,
-    D: 500,
-    M: 1000
-  };
-
   while (roman.length) {
-    val = lookup[roman.shift()];
-    num += val * (val < lookup[roman[0]] ? -1 : 1);
+    val = numerals[roman.shift()];
+    num += val * (val < numerals[roman[0]] ? -1 : 1);
   }
-
   return num;
 };
